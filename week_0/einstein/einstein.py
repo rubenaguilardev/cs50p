8d895ffbb60ed40eb05471e14931a1c51b8be62a
rubenaguilardev <ragui56@wgu.edu>
2025-07-02 02:25:43 -0700

    einstein solution

---
+++ week_0/einstein/einstein.py
@@ -0,0 +1,24 @@
+"""
+Even if you haven’t studied physics (recently or ever!), you might have heard that E = mc2,
+wherein E represents energy (measured in Joules), represents mass (measured in kilograms), and 
+represents the speed of light (measured approximately as 300000000 meters per second), 
+per Albert Einstein et al. Essentially, the formula means that mass and energy are equivalent.
+
+In a file called einstein.py, implement a program in Python that prompts the user for mass as an 
+integer (in kilograms) and then outputs the equivalent number of Joules as an integer. Assume that the 
+user will input an integer.
+"""
+
+
+def main():
+
+    m = int(input('m: '))
+    c = 300000000
+
+    e = m * (c * c)
+
+    print(f"E: {e}")
+
+main()
+
+
